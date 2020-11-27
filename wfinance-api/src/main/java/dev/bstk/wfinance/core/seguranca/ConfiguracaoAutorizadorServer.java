2205647f1b996caafb4bb8bab465882b311132d7
bruno-luz <bruno.luz@preventsenior.com.br>
2020-11-27 16:59:38 -0300

    main - Configurando Spring Security

---
+++ wfinance-api/src/main/java/dev/bstk/wfinance/core/seguranca/ConfiguracaoAutorizadorServer.java
@@ -0,0 +1,10 @@
+package dev.bstk.wfinance.core.seguranca;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class ConfiguracaoAutorizadorServer {
+
+}
--- wfinance-api/src/main/java/dev/bstk/wfinance/core/seguranca/ConfiguracaoResourceServer.java
+++ wfinance-api/src/main/java/dev/bstk/wfinance/core/seguranca/ConfiguracaoResourceServer.java
@@ -1,34 +1,36 @@
 package dev.bstk.wfinance.core.seguranca;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class ConfiguracaoSeguranca extends WebSecurityConfigurerAdapter {
+public class ConfiguracaoResourceServer extends ResourceServerConfigurerAdapter {
 
-    @Bean
-    public PasswordEncoder bcryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ConfiguracaoResourceServer(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
     }
 
-    @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+    @Autowired
+    public void configure(final AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
             .withUser("admin")
-            .password(bcryptPasswordEncoder().encode("admin"))
+            .password(passwordEncoder.encode("admin"))
             .roles("ROLE");
     }
 
     @Override
-    protected void configure(final HttpSecurity http) throws Exception {
+    public void configure(final HttpSecurity http) throws Exception {
         http.authorizeRequests()
             .antMatchers("/**/categorias/**")
             .permitAll()
@@ -42,5 +44,10 @@ public class ConfiguracaoSeguranca extends WebSecurityConfigurerAdapter {
             .and()
             .csrf()
             .disable();
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.stateless(Boolean.FALSE);
     }
 }
