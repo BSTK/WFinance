a121f5a8139e9baa63c7782e63f00c81395c7ff7
Bruno Luz <bsilva.se@gmail.com>
2021-01-22 21:10:16 -0300

    main - Configurando email com Thymeleaf

--- wfinance-api/src/main/java/dev/bstk/wfinance/core/email/EnvioEmail.java
+++ wfinance-api/src/main/java/dev/bstk/wfinance/core/email/EnvioEmail.java
@@ -5,19 +5,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import javax.mail.MessagingException;
 import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
 
 @Slf4j
 @Component
 public class EnvioEmail {
 
     private final JavaMailSender mailSender;
+    private final TemplateEngine templateEngine;
 
     @Autowired
-    public EnvioEmail(final JavaMailSender mailSender) {
+    public EnvioEmail(final JavaMailSender mailSender,
+                      final TemplateEngine templateEngine) {
         this.mailSender = mailSender;
+        this.templateEngine = templateEngine;
     }
 
     public void enviar(final Email email) {
@@ -35,6 +42,21 @@ public class EnvioEmail {
             log.error("Nao foi possivél enviar email. Ex = {}", ex.getMessage(), ex);
             throw new RuntimeException(ex);
         }
+    }
+
+    public void enviar(final Email email, final String templateHtml, final Map<String, Object> parametros) {
+        final var context = new Context(Locale.forLanguageTag("pt_BR"));
+        parametros.forEach(context::setVariable);
+
+        final var emailMensagemHtml = templateEngine.process(templateHtml, context);
+        final var emailComHtml = new Email(
+            email.getAssunto(),
+            email.getRemetente(),
+            emailMensagemHtml,
+            email.getDestinatarios()
+        );
+
+        enviar(emailComHtml);
     }
 
 }
